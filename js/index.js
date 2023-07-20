78209c5dba7ee01e2e6d1f03b25cd944bcd3426a
ilkay <06ilkayozturk@gmail.com>
2023-07-20 22:39:49 +0300

    home slidebar js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 // ! home slidebar start
 const btnOpenSidebar = document.querySelector("#btn-menu");
 const sidebar = document.querySelector("#sidebar");
+const btnCloseSidebar = document.querySelector("#close-sidebar");
 btnOpenSidebar.addEventListener("click", function () {
   sidebar.style.left = "0";
-  sidebar.style.zIndenx = "1";
+});
+
+btnCloseSidebar.addEventListener("click", function () {
+  sidebar.style.left = "-100%";
 });
 
 // ! home slidebar end
