7ac41c8b88af7689c3bfc54de8182de61652286f
ilkay <06ilkayozturk@gmail.com>
2023-07-22 11:29:13 +0300

    slider dosts

--- js/index.js
+++ js/index.js
@@ -62,8 +62,13 @@ function plusSlide(n) {
   showSlides((slideIndex += n));
 }
 
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
 function showSlides(n) {
   const slides = document.getElementsByClassName("slider-item");
+  const dots = document.getElementsByClassName("slider-dot");
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -74,7 +79,10 @@ function showSlides(n) {
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " active";
 }
 // ! slider end
