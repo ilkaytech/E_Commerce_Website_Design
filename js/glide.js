ce8fecffcbf576cd2d1ad77c2b2812544f0838ed
ilkay <06ilkayozturk@gmail.com>
2023-07-22 13:06:57 +0300

    glide responsive

--- js/glide.js
+++ js/glide.js
@@ -1,4 +1,19 @@
-new Glide(".glide", {
+const config = {
   type: "carousel",
   perView: 4,
-}).mount();
+  gap: 20,
+  autoplay: 3000,
+  breakpoints: {
+    992: {
+      perView: 3,
+    },
+    768: {
+      perView: 2,
+    },
+    576: {
+      perView: 1,
+    },
+  },
+};
+
+new Glide(".product-carousel", config).mount();
